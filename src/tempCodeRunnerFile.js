460ec6599e57533363c56205a78b0a04ec7dd658
Ankush <av479175@gmail.com>
2026-09-11 14:10:32 +0530

    chore: remove temporary code runner file and add .gitignore for node_modules and .env

--- src/tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config'; // This automatically runs .config() immediately
-import mongoose from "mongoose";
-
-import { DB_NAME } from "./constants";
-import connectDB from ".db/index.js";
-
-dotenv.config({ path: './.env' })
-
-connectDB()
