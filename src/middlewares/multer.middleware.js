734994cc5c383ddc499a824d87d2fd5932131672
Ankush <av479175@gmail.com>
2026-09-12 02:58:40 +0530

    feat: implement cloudinary upload utility and multer middleware for file handling

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,20 @@
+import multer from "multer";
+
+//its a middleware
+
+const storage = multer.diskStorage({
+	// Choose the folder where uploaded files will be stored.
+	destination: function (req, file, cb) {
+		cb(null, "./public/temp");
+	},
+
+	// Give each uploaded file a unique name while keeping its original extension.
+	filename: function (req, file, cb) {
+		// const uniqueName = `${Date.now()}-${file.originalname}`;
+		cb(null, file.originalname);
+	}
+});
+
+const upload = multer({ storage });
+
+export { upload };
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,1 +1,33 @@
-import {v2 as cloudinary} from "cloudinary";
+import { v2 as cloudinary } from "cloudinary";
+import { log } from "console";
+import fs from "fs";
+
+// Configuration
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath)=>{
+    try {
+        if(!localFilePath){
+            //there is not file path exist
+            console.log('file path do not exist');
+            return null;
+        }
+        //upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath , {
+            resource_type : "auto"
+        } )
+        //file has been uplaoded successfully
+        console.log("file is upladed to cloudinary ", response.url);
+        //public url to upladed file
+        return response;
+    }catch (err){
+        fs.unlinkSync(localFilePath); // remove the loacally saved file as the uplaod operation got failed
+        return null;
+    }
+}
+
+export {uploadOnCloudinary}
