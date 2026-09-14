baeb50e4213e8e4365cd44a25c5acea47cba3052
Ankush <av479175@gmail.com>
2026-09-14 23:02:12 +0530

    feat: remove temporary screenshot files and update cloudinary upload logging

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -21,8 +21,10 @@ const uploadOnCloudinary = async (localFilePath)=>{
             resource_type : "auto"
         } )
         //file has been uplaoded successfully
-        console.log("file is upladed to cloudinary ", response.url);
+        // console.log("file is upladed to cloudinary ", response.url);
         //public url to upladed file
+        fs.unlinkSync(localFilePath);
+        //error aya toh bhi remove hojayegi to success par bhi
         return response;
     }catch (err){
         fs.unlinkSync(localFilePath); // remove the loacally saved file as the uplaod operation got failed
